061d540e71777674745e7abe926d666d20ff3853
vitaly_vm@bk.ru <ne3Vubeki83410950921>
2019-10-31 15:49:02 +0300

    Revert "task 6"
    This reverts commit ff450e73

--- ra/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import {Tick} from "./components/tick";
-
-ReactDOM.render(
-    <Tick/>,
-    document.getElementById("root")
-);
